0416a56b193f79fe33db1b8641cef5c46f3c8818
SangeethaCovalsys <sangeetham@covalsys.com>
2026-04-06 17:14:18 +0530

    all api are added via store procedure and posting throughb service layer -06-4-2026

---
+++ TTSteelAndroidAPI/Interface/IProductionExecutionService.cs
@@ -0,0 +1,9 @@
+﻿using static TTSteelAndroidAPI.Model.ProductionExecution.Production;
+
+namespace TTSteelAndroidAPI.Interface
+{
+    public interface IProductionExecutionService
+    {
+       Task<UnitCountsDto> GetUnitCountsAsync();
+    }
+}
